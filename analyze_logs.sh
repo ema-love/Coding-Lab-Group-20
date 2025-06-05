c9bd146b6f646541caff04cf05a09cd2d43cefb8
ema-love <e.akintayo@alustudent.com>
2025-06-04 19:17:01 -0700

    Added log analysis script and sample log file

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -13,9 +13,45 @@ while true; do
     #use of conditional or statements, we could potentially use the regex sysntax i.e if [[ "$user_choice" =~ ^[1-3]$ ]] ; then
     if [[ "$user_choice" == "1" || "$user_choice" == "2" || "$user_choice" == "3" ]]; then
     	echo " youve chosen $user_choice please proceed"
-	 break
-    else
-        echo "invalid output, please output a number between 1-3"
-    fi
 
+	   case $user_choice in
+            1) LOG_FILE="hospital_data/active_logs/heart_rate.log" ;;
+            2) LOG_FILE="hospital_data/active_logs/temperature.log" ;;
+            3) LOG_FILE="hospital_data/active_logs/water_usage.log" ;;
+        esac
+
+        break
+    else
+        echo "Invalid input. Please enter a number between 1 and 3."
+    fi
+done
+	
+
+
+
+if [[ ! -f "$LOG_FILE" ]]; then
+        echo "log_file not found: $LOG_FILE"
+        exit 1
+fi
+
+echo "Analysis report for $LOG_FILE"
+echo"............"
+
+DEVICES=$(awk '{print $2}' "$LOG_FILE" | sort | uniq)
+for device in $DEVICES; do
+        echo "Device: $devics"
+
+
+        COUNT=$(grep "$device" "$LOG_FILE" | wc -l)
+        echo "Total Entries: $COUNT"
+
+
+
+
+         FIRST=$(grep "$device" "$LOG_FILE" | head -1 | awk '{print $1}')
+  echo "  First Entry: $FIRST"
+
+  LAST=$(grep "$device" "$LOG_FILE" | tail -1 | awk '{print $1}')
+  echo "  Last Entry: $LAST"
+  echo ""
 done
